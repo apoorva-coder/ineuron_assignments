d4a2e02b0875dbbdaf4503c6d89bc3bee7e60577
Apoorva.R <apoorvarrao@gmail.com>
2021-05-18 13:49:17 +0530

    Add files via upload

---
+++ Assignment-1/Q-2.py
@@ -0,0 +1,9 @@
+"""
+Write a python program to accept the user's first and last name and
+then getting them printed in the reverse order with a space between
+first name and last name.
+"""
+Fname = input("Enter your first_name: ")
+Lname = input("Enter your last_name: ")
+name = Fname + "\t" + Lname
+print(name)
---
+++ Assignment-1/Q-3.py
@@ -0,0 +1,7 @@
+"""
+Write a Python program to find the volume of the sphere with
+diameter 12cm.
+"""
+r = 12
+Volume = round((4/3)*(3.14)*(r**3),2)
+print("Volume is:",Volume,"cm^3")
